eafdaf46ecef77f3263474d22a24cae10192a738
fwend <josvanuden@gmail.com>
2023-05-21 16:43:24 +0200

    arrays

--- nodes/MD5Node/MD5Node.node.ts
+++ nodes/MD5Node/MD5Node.node.ts
@@ -10,6 +10,17 @@ import type {BinaryToTextEncoding} from 'crypto';
 import {createHash} from 'crypto';
 import {deepCopy} from 'n8n-workflow';
 import set from 'lodash.set';
+import get from 'lodash.get';
+
+interface IKeyValuePair {
+	val: string;
+	key: string
+}
+
+interface IPathKeysPair {
+	path: string;
+	keys: string
+}
 
 export class MD5Node implements INodeType {
 	description: INodeTypeDescription = {
@@ -27,20 +38,83 @@ export class MD5Node implements INodeType {
 			// Node properties which the user gets displayed and
 			// can change on the node.
 			{
-				displayName: 'Value',
-				name: 'val',
-				type: 'string',
+				displayName:
+					'You have been put on notice',
+				name: 'infoBox',
+				type: 'notice',
 				default: '',
-				placeholder: '',
-				description: 'Value to be hashed',
 			},
 			{
-				displayName: 'Key',
-				name: 'key',
-				type: 'string',
-				default: '',
-				placeholder: '',
-				description: 'Key to store the result',
+				displayName: 'Fields to Hash',
+				name: 'fieldsToHash',
+				type: 'fixedCollection',
+				placeholder: 'Add Fields to Hash',
+				default: {values: [{val: '', key: ''}]},
+				typeOptions: {
+					multipleValues: true,
+				},
+				options: [
+					{
+						displayName: 'Values',
+						name: 'values',
+						values: [
+							{
+								displayName: 'Value',
+								name: 'val',
+								type: 'string',
+								default: '',
+								// eslint-disable-next-line n8n-nodes-base/node-param-placeholder-miscased-id
+								placeholder: '',
+								hint: 'Use an expression to capture the value',
+							},
+							{
+								displayName: 'Key',
+								name: 'key',
+								type: 'string',
+								default: '',
+								// eslint-disable-next-line n8n-nodes-base/node-param-placeholder-miscased-id
+								placeholder: '',
+								hint: 'This is the output path, may be the same as input path',
+							},
+						],
+					},
+				],
+			},
+			{
+				displayName: 'Arrays to Hash',
+				name: 'arraysToHash',
+				type: 'fixedCollection',
+				placeholder: 'Add Arrays to Hash',
+				default: {values: [{path: '', keys: ''}]},
+				typeOptions: {
+					multipleValues: true,
+				},
+				options: [
+					{
+						displayName: 'Arrays',
+						name: 'values',
+						values: [
+							{
+								displayName: 'Path',
+								name: 'path',
+								type: 'string',
+								default: '',
+								// eslint-disable-next-line n8n-nodes-base/node-param-placeholder-miscased-id
+								placeholder: '',
+								hint: 'Path to the array',
+							},
+							{
+								displayName: 'Keys',
+								name: 'keys',
+								type: 'string',
+								default: '',
+								// eslint-disable-next-line n8n-nodes-base/node-param-placeholder-miscased-id
+								placeholder: '',
+								hint: 'Keys to hash',
+							},
+						],
+					},
+				],
 			},
 		],
 	};
@@ -50,7 +124,6 @@ export class MD5Node implements INodeType {
 		const returnData: INodeExecutionData[] = [];
 
 		let item: INodeExecutionData;
-		let key: string, val: string;
 		let newItem: INodeExecutionData;
 
 		// Iterates over all input items
@@ -58,30 +131,42 @@ export class MD5Node implements INodeType {
 			try {
 				item = items[itemIndex];
 
-				key = this.getNodeParameter('key', itemIndex, '') as string;
-				val = this.getNodeParameter('val', itemIndex, '') as string;
+				newItem = {
+					json: deepCopy(item.json),
+					pairedItem: {
+						item: itemIndex,
+					},
+				};
 
-				if (key && val) {
-					const newValue = createHash('MD5').update(val).digest('HEX' as BinaryToTextEncoding);
-					if (key.includes('.')) {
-						// Uses dot notation so copy all data
-						newItem = {
-							json: deepCopy(item.json),
-							pairedItem: {
-								item: itemIndex,
-							},
-						};
-					} else {
-						newItem = {
-							json: {...item.json},
-							pairedItem: {
-								item: itemIndex,
-							},
-						};
+				const values = this.getNodeParameter('fieldsToHash.values', itemIndex, '') as Array<IKeyValuePair>
+				for (const { val, key } of values || []) {
+
+					if (val && key) {
+						const newValue = createHash('MD5').update(val).digest('HEX' as BinaryToTextEncoding);
 						set(newItem, `json.${key}`, newValue);
-						returnData.push(newItem)
 					}
 				}
+
+				const arrays = this.getNodeParameter('arraysToHash.values', itemIndex, '') as Array<IPathKeysPair>
+				for (const { path, keys } of arrays || []) {
+
+					if (path && keys) {
+						  const arr = get(item, `json.${path}`) as Array<any>
+						  if (arr && arr.length) {
+								 const keysToHash = keys.split(',').map(s => s.trim()) as Array<string>
+								 for (const keyToHash of keysToHash) {
+									   const obj = arr[0]
+									   if (obj[keyToHash]) {
+											 const newValue = createHash('MD5').update(obj[keyToHash]).digest('HEX' as BinaryToTextEncoding);
+											 set(newItem, `json.${path}[0].${keyToHash}`, newValue);
+										 }
+								 }
+							}
+					}
+				}
+
+				returnData.push(newItem)
+
 			} catch (error) {
 
 				if (this.continueOnFail()) {
